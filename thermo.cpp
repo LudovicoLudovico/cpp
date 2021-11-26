415457c15efb25c16ab211525e2a23337552c17d
Ludovico Furlanetto <ludovico.furlanetto@gmail.com>
2021-11-26 16:07:51 +0100

    added a and b parameters

--- thermo.cpp
+++ thermo.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <math.h>
+#include <cmath>
 
 // Calculate mean
 double mean(std::vector<double> &vect)
@@ -81,54 +81,121 @@ double variance(std::vector<double> &vect)
     return (calculated_standard_deviations * calculated_standard_deviations);
 }
 
+std::vector<double> calculate_a_b(std::vector<double> &x, std::vector<double> &y, double sigma_y)
+{
+    double sum_x;
+    double sum_x_squared;
+    double sum_y;
+    double sum_x_y;
+    double a;
+    double b;
+    int i = 0;
+
+    for (auto &sx : x)
+    {
+        sum_x += sx;
+        sum_x_squared += sx * sx;
+        sum_y += y[i];
+        sum_x_y += sx * y[i];
+        ++i;
+    }
+
+    a = (sum_x_squared * sum_y - sum_x * sum_x_y) / (x.size() * sum_x_squared - (sum_x * sum_x));
+    b = (x.size() * sum_x_y - sum_x * sum_y) / (x.size() * sum_x_squared - (sum_x * sum_x));
+
+    double sigma_a = sigma_y * sqrt(sum_x_squared / (x.size() * sum_x_squared - (sum_x * sum_x)));
+    double sigma_b = sigma_y * sqrt(x.size() / (x.size() * sum_x_squared - (sum_x * sum_x)));
+
+    return {
+        a, sigma_a, b, sigma_b};
+}
+
 int main()
 {
 
-    std::cout << "Select one of these functionalities: \n (1) - Calculate the mean \n (2) - Calculate the center of interval \n (3) - Parital dispertion \n (4) - Standard deviation \n (5) - Variance" << std::endl;
+    std::cout << "Select one of these functionalities: \n (1) - Calculate the mean \n (2) - Calculate the center of interval \n (3) - Parital dispertion \n (4) - Standard deviation \n (5) - Variance \n (6) - Linear Fit (A and B)" << std::endl;
 
     int user_choice;
     std::cin >> user_choice;
 
-    // Let the user insert its data
-    std::vector<double> data;
-    std::cout << "Insert your data (Enter a letter to stop inserting): \n";
-
-    double user_data;
-    while (std::cin >> user_data)
+    if (user_choice != 6)
     {
-        data.push_back(user_data);
-    }
+        std::vector<double> data;
+        std::cout << "Insert your data (Enter a letter to stop inserting): \n";
 
-    double result;
-    std::string operation = "";
-    switch (user_choice)
+        double user_data;
+        while (std::cin >> user_data)
+        {
+            data.push_back(user_data);
+        }
+
+        double result;
+        std::string operation = "";
+        switch (user_choice)
+        {
+        case 1:
+            result = mean(data);
+            operation = "mean";
+            break;
+        case 2:
+            result = interval_center(data);
+            operation = "center of the interval";
+            break;
+        case 3:
+            result = partial_dispertion(data);
+            operation = "partial dispertion";
+            break;
+        case 4:
+            result = standard_deviation(data);
+            operation = "standard deviation";
+            break;
+        case 5:
+            result = variance(data);
+            operation = "variance";
+            break;
+        default:
+            return EXIT_FAILURE;
+            break;
+        }
+
+        std::cout << "The " << operation << " of your data set is:  " << result << std::endl;
+    }
+    else
     {
-    case 1:
-        result = mean(data);
-        operation = "mean";
-        break;
-    case 2:
-        result = interval_center(data);
-        operation = "center of the interval";
-        break;
-    case 3:
-        result = partial_dispertion(data);
-        operation = "partial dispertion";
-        break;
-    case 4:
-        result = standard_deviation(data);
-        operation = "standard deviation";
-        break;
-    case 5:
-        result = variance(data);
-        operation = "variance";
-        break;
-    default:
-        return EXIT_FAILURE;
-        break;
-    }
+        std::vector<double> x;
+        std::cout << "Insert values for x: \n";
+        double x_ins;
+        while (std::cin >> x_ins)
+        {
+            x.push_back(x_ins);
+        };
 
-    std::cout << "The " << operation << " of your data set is:  " << result << std::endl;
+        std::cin.clear();
+        std::cin.ignore(1, '\n');
+
+        std::vector<double> y;
+        std::cout << "Insert values for y: \n";
+        double y_ins;
+        while (std::cin >> y_ins && y.size() < x.size())
+        {
+            y.push_back(y_ins);
+        };
+
+        std::cin.clear();
+        std::cin.ignore(1, '\n');
+
+        std::cout << "Insert the error on y values: ";
+        double sigma_y;
+        std::cin >> sigma_y;
+
+        if (std::cin.fail())
+        {
+            return EXIT_FAILURE;
+        }
+
+        std::vector result = calculate_a_b(x, y, sigma_y);
+        std::cout << "\nA: " << result[0] << " ± " << result[1] << "\nB: " << result[2] << " ± " << result[3] << std::endl;
+    }
 
     return 0;
 }
