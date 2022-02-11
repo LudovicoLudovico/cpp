290d6a07dfb27987249c0c2b722b1b67a4e28581
Ludovico Furlanetto <ludovico.furlanetto@gmail.com>
2022-02-11 17:15:18 +0100

    added simple calc

---
+++ simpleCalculator.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <stdexcept>
+#include <string>
+
+int main()
+{
+   try
+   {
+      std::cout << "First operand ";
+      double a;
+      std::cin >> a;
+      if (!std::cin.good())
+         throw std::runtime_error{"Invalid First Operand"};
+
+      std::cout << "Second operand ";
+      double b;
+      std::cin >> b;
+
+      if (!std::cin.good())
+         throw std::runtime_error{"Invalid Second Operand"};
+
+      std::cout << "Operator ( + , - , / , * , %) ";
+      char operation;
+      std::cin >> operation;
+
+      double result;
+      switch (operation)
+      {
+      case '+':
+         result = a + b;
+         break;
+      case '-':
+         result = a - b;
+         break;
+      case '/':
+         result = a / b;
+         break;
+      case '*':
+         result = a * b;
+         break;
+      case '%':
+         result = a * b;
+         break;
+      default:
+         std::string error = "Invalid Operator: ";
+         throw std::runtime_error{error + operation};
+         break;
+      }
+      std::cout << "The result is: " << result << '\n';
+   }
+   catch (std::runtime_error const &e)
+   {
+      std::cerr << e.what() << '\n';
+   }
+}
